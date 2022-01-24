67c5bd595d22cc399e56301e7bc6a75bedf9fe19
ahit976 <55730608+ahit976@users.noreply.github.com>
2022-01-24 16:55:42 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    microIoT.microIoT_SendMessage("close-curtain", microIoT.TOPIC.topic_0)
+    microIoT.microIoT_showUserText(1, "")
+    microIoT.microIoT_showUserText(1, "curtain closed")
+})
 function 显示数字 (数字: number) {
     if (数字 >= 0 && 数字 <= 9) {
         basic.showNumber(数字)
@@ -93,18 +98,23 @@ function 显示数字 (数字: number) {
 }
 microIoT.microIoT_MQTT_Event(microIoT.TOPIC.topic_0, function (message) {
     if (message == "L") {
-        microIoT.microIoT_showUserText(0, "DFRobot")
+        microIoT.microIoT_showUserText(0, "recved L ")
     }
 })
 input.onButtonPressed(Button.A, function () {
     microIoT.microIoT_SendMessage("livingroom-lightbelt-toggle", microIoT.TOPIC.topic_0)
     microIoT.microIoT_showUserText(1, "")
-    microIoT.microIoT_showUserText(1, "close-curtain")
+    microIoT.microIoT_showUserText(1, "lightbelt toggled")
+})
+input.onButtonPressed(Button.AB, function () {
+    microIoT.microIoT_SendMessage("open-curtain", microIoT.TOPIC.topic_0)
+    microIoT.microIoT_showUserText(1, "")
+    microIoT.microIoT_showUserText(1, "curtain opened")
 })
 input.onButtonPressed(Button.B, function () {
     microIoT.microIoT_SendMessage("livingroom-mainlight-toggle", microIoT.TOPIC.topic_0)
-    microIoT.microIoT_showUserText(2, "")
-    microIoT.microIoT_showUserText(2, "close-curtain")
+    microIoT.microIoT_showUserText(1, "")
+    microIoT.microIoT_showUserText(1, "mainlight toggled")
 })
 function 亮灯 (编号: number, 时长: number, 颜色: number) {
     rt = 时长
